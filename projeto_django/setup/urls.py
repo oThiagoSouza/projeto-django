00126c9aa078337b8069e13510d1e7d6c5ba7e23
Thiago Souza <111827538+oThiagoSouza@users.noreply.github.com>
2025-03-04 22:09:49 -0300

    Delete projeto_django directory

--- projeto_django/setup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from todos.views import todo_list
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", todo_list),
-]
